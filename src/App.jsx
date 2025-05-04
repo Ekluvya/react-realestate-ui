8fdc4c83e17086ded787c60709851a4ca9fcfcfa
ekluvya <47681673+Ekluvya@users.noreply.github.com>
2025-05-05 00:10:52 +0530

    added new pages and fixed deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
 import Layout from "./layout/layout";
 import HomePage from "./routes/homePage/homePage";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import SinglePage from "./routes/singlePage/singlePage";
 
 function App() {
-  const router = createBrowserRouter([
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/react-realestate-ui/",
+  //     element: <Layout />,
+  //     children: [
+  //       {
+  //         path: "",
+  //         element: <HomePage />,
+  //       },
+  //       {
+  //         path: "list",
+  //         element: <ListPage />,
+  //       },
+  //       {
+  //         path: ":id",
+  //         element: <SinglePage />,
+  //       },
+  //     ],
+  //   },
+  // ]);
+
+  const router = createHashRouter([
     {
-      path: "/react-realestate-ui/",
+      path: "/",
       element: <Layout />,
       children: [
-        {
-          path: "",
-          element: <HomePage />,
-        },
-        {
-          path: "list",
-          element: <ListPage />,
-        },
-        {
-          path: ":id",
-          element: <SinglePage />,
-        },
+        { path: "", element: <HomePage /> },
+        { path: "list", element: <ListPage /> },
+        { path: ":id", element: <SinglePage /> },
       ],
     },
   ]);
